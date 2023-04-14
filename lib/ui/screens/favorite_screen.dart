9bf7b6413115e9fc7086d84676ff6549d542e567
Osman Asanov <anilf.business@gmail.com>
2023-04-15 01:29:16 +0300

    fav final

--- lib/ui/screens/favorite_screen.dart
+++ lib/ui/screens/favorite_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../data/app_data.dart';
+import '../../data/models/food.dart';
+import '../../ui_kit/app_color.dart';
+import '../../ui_kit/app_icon.dart';
 import '../widgets/empty_wrapper.dart';
 
 class FavoriteScreen extends StatefulWidget {
@@ -10,6 +14,7 @@ class FavoriteScreen extends StatefulWidget {
 }
 
 class FavoriteScreenState extends State<FavoriteScreen> {
+  var favoriteFood = AppData.favoriteItems;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +22,8 @@ class FavoriteScreenState extends State<FavoriteScreen> {
       body: EmptyWrapper(
         type: EmptyWrapperType.favorite,
         title: 'Empty favorite',
-        isEmpty: true,
-        child: Container(),
+        isEmpty: favoriteFood.isEmpty,
+        child: _favoriteListView(),
       ),
     );
   }
@@ -30,4 +35,38 @@ class FavoriteScreenState extends State<FavoriteScreen> {
       ),
     );
   }
+
+  Widget _favoriteListView() {
+  return ListView.separated(
+    padding: const EdgeInsets.all(30),
+    itemCount: favoriteFood.length,
+    itemBuilder: (_, index) {
+      Food food = favoriteFood[index];
+      return Card(
+        color: Theme.of(context).brightness == Brightness.light ? Colors.white : 
+        DarkThemeColor.primaryLight,
+        shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: ListTile(
+          title: Text(
+            food.name,
+            style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        leading: Image.asset(food.image),
+        subtitle: Text(
+          food.description,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        trailing: const Icon(AppIcon.heart, color: Colors.redAccent),
+      ),
+      );
+    },
+      separatorBuilder: (_, __) => Container(
+      height: 20,
+    ),
+  );
+}
+
 }

00554408d53798fa228f591a5e7c8a975656e32d
Osman Asanov <anilf.business@gmail.com>
2023-04-14 23:41:24 +0300

    food widgets

--- lib/ui/widgets/food_list_view.dart
+++ lib/ui/widgets/food_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/ui_kit/app_color.dart';
 import '../../data/models/food.dart';
+import '../../ui_kit/app_text_style.dart';
 
 class FoodListView extends StatelessWidget {
   const FoodListView({super.key, required this.foods, this.isReversed = false});
@@ -18,12 +19,41 @@ class FoodListView extends StatelessWidget {
           scrollDirection: Axis.horizontal,
           padding: const EdgeInsets.only(top: 20),
           itemBuilder: (_, index) {
-            return Container(
-              width: 160,
-              decoration: BoxDecoration(
-                color: isDark ? 
-                DarkThemeColor.primaryLight : Colors.white,
-                borderRadius: const BorderRadius.all(Radius.circular(20)),
+            // ignore: unused_local_variable
+            Food food =
+                isReversed ? foods.reversed.toList()[index] : foods[index];
+            return GestureDetector(
+              onTap: () {
+                // ignore: avoid_print
+                print('Клик на карточку');
+              },
+              child: Container(
+                width: 160,
+                decoration: BoxDecoration(
+                  color: isDark ? DarkThemeColor.primaryLight : Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(food.image, scale: 6),
+                      Text(
+                        "\$${food.price}",
+                        style: AppTextStyle.h3Style
+                            .copyWith(color: LightThemeColor.accent),
+                      ),
+                      Text(
+                        food.name,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             );
           },

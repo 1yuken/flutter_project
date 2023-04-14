c04943c843241721a6022fbad23b1494d6eef67c
Osman Asanov <anilf.business@gmail.com>
2023-04-15 01:21:01 +0300

    favorite screen

--- lib/ui/screens/favorite_screen.dart
+++ lib/ui/screens/favorite_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/empty_wrapper.dart';
+
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
 
@@ -10,6 +12,22 @@ class FavoriteScreen extends StatefulWidget {
 class FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: _appBar(context),
+      body: EmptyWrapper(
+        type: EmptyWrapperType.favorite,
+        title: 'Empty favorite',
+        isEmpty: true,
+        child: Container(),
+      ),
+    );
+  }
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      title: Text(
+        "Favorite screen",
+        style: Theme.of(context).textTheme.displayMedium,
+      ),
+    );
   }
 }

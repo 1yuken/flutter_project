9f1548b02f0f79f3b3e9f7d89f3647ea431a2d36
Osman Asanov <anilf.business@gmail.com>
2023-04-14 23:17:22 +0300

    theme settings

--- lib/ui/widgets/food_list_view.dart
+++ lib/ui/widgets/food_list_view.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/ui_kit/app_color.dart';
 import '../../data/models/food.dart';
 
 class FoodListView extends StatelessWidget {
-  const FoodListView({
-super.key, 
-required this.foods, 
-this.isReversed = false
-  });
+  const FoodListView({super.key, required this.foods, this.isReversed = false});
 
   final List<Food> foods;
   final bool isReversed;
 
   @override
   Widget build(BuildContext context) {
+    // ignore: unused_local_variable
+    final isDark = Theme.of(context).brightness == Brightness.dark;
     return SizedBox(
       height: 200,
       child: ListView.separated(
@@ -21,9 +20,10 @@ this.isReversed = false
           itemBuilder: (_, index) {
             return Container(
               width: 160,
-              decoration: const BoxDecoration(
-                color: Colors.orange,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
+              decoration: BoxDecoration(
+                color: isDark ? 
+                DarkThemeColor.primaryLight : Colors.white,
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
               ),
             );
           },

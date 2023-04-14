fa654f7bf907a20b9fc3b6317f036401c71719d1
Osman Asanov <anilf.business@gmail.com>
2023-04-14 15:20:22 +0300

    search widget

--- lib/ui/screens/food_list_screen.dart
+++ lib/ui/screens/food_list_screen.dart
@@ -13,53 +13,55 @@ class FoodList extends StatefulWidget {
 class FoodListState extends State<FoodList> {
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: _appBar(context),
-    body: Padding(
-      padding: const EdgeInsets.all(20),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Morning, Yuken",
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            Text(
-              "What do you want to eat \ntoday",
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-            Text(
-              "Available for you",
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 25, bottom: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Best food of the week",
-                    style: Theme.of(context).textTheme.displaySmall,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: Text(
-                      "See all",
-                      style:
-                      Theme.of(context).textTheme.headlineMedium?.copyWith(color:LightThemeColor.accent),
-                    ),
-                  ),
-                ],
+      appBar: _appBar(context),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Morning, Yuken",
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
-            ),
-          ],
+              Text(
+                "What do you want to eat \ntoday",
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              _searchBar(),
+              Text(
+                "Available for you",
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 25, bottom: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Best food of the week",
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Text(
+                        "See all",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium
+                            ?.copyWith(color: LightThemeColor.accent),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    )
-  );
+      ));
 }
 
-PreferredSizeWidget _appBar(BuildContext context){
+PreferredSizeWidget _appBar(BuildContext context) {
   return AppBar(
     leading: IconButton(
       icon: const FaIcon(FontAwesomeIcons.dice),
@@ -69,7 +71,8 @@ PreferredSizeWidget _appBar(BuildContext context){
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const Icon(Icons.location_on_outlined, color: LightThemeColor.accent),
-        Text("Location",
+        Text(
+          "Location",
           style: Theme.of(context).textTheme.bodyLarge,
         )
       ],
@@ -78,20 +81,28 @@ PreferredSizeWidget _appBar(BuildContext context){
       IconButton(
         onPressed: () {},
         icon: Badge(
-          badgeStyle: const BadgeStyle(
-              badgeColor: LightThemeColor.accent
-          ),
+          badgeStyle: const BadgeStyle(badgeColor: LightThemeColor.accent),
           badgeContent: const Text(
             "2",
             style: TextStyle(color: Colors.white),
           ),
-          position: BadgePosition.topStart(
-              start: -3
-          ),
-          child: const Icon(Icons.notifications_none,
-              size: 30),
+          position: BadgePosition.topStart(start: -3),
+          child: const Icon(Icons.notifications_none, size: 30),
         ),
       )
     ],
   );
 }
+
+Widget _searchBar() {
+  return const Padding(
+    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: 'Search food',
+        prefixIcon: Icon(Icons.search, color: Colors.grey),
+      ),
+    ),
+  );
+}
+
--- lib/ui_kit/app_theme.dart
+++ lib/ui_kit/app_theme.dart
@@ -23,8 +23,22 @@ class AppTheme {
       bodyLarge: AppTextStyle.bodyTextLight,
       titleMedium: AppTextStyle.subtitleLight,
     ),
+      hintColor: Colors.black45,
+      inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(borderSide: BorderSide.none),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        borderSide: BorderSide(color: Colors.transparent),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        borderSide: BorderSide(color: Colors.transparent),
+      ),
+      filled: true,
+      contentPadding: EdgeInsets.all(20),
+      fillColor: Colors.white,
+    ),
   );
-
   static ThemeData darkTheme = ThemeData(
     scaffoldBackgroundColor: DarkThemeColor.primaryDark,
     appBarTheme: AppBarTheme(
@@ -43,6 +57,20 @@ class AppTheme {
       headlineSmall: AppTextStyle.h5StyleLight.copyWith(color: Colors.white),
       bodyLarge: AppTextStyle.bodyTextLight.copyWith(color: Colors.white),
       titleMedium: AppTextStyle.subtitleLight.copyWith(color: Colors.white60),
+    ),
+      hintColor: Colors.white60,
+      inputDecorationTheme: const InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        borderSide: BorderSide(color: Colors.transparent),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        borderSide: BorderSide(color: Colors.transparent),
+      ),
+      filled: true,
+      contentPadding: EdgeInsets.all(20),
+      fillColor: DarkThemeColor.primaryLight,
     ),
   );
 }

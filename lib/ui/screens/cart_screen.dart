c9aa74ab4dca6db41513d4a151a020dbf7f354d8
Osman Asanov <anilf.business@gmail.com>
2023-04-14 23:56:05 +0300

    cart design

--- lib/ui/screens/cart_screen.dart
+++ lib/ui/screens/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/ui/widgets/empty_wrapper.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -10,8 +11,13 @@ class CartScreen extends StatefulWidget {
 class CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: _appBar(context),
+      body: EmptyWrapper(
+        title: "Empty cart",
+        isEmpty: true,
+        child: Container(),
+      ),
     );
   }
     PreferredSizeWidget _appBar(BuildContext context) {
